ff135dde9a92b03d691331083b98e6e33bf14178
mud00 <96502981+mud00@users.noreply.github.com>
2022-04-15 14:53:07 +0200

    Update index.js

--- crud/server/index.js
+++ crud/server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    password: 'arcatem2k22',
+    password: 'password',
     database: 'employeeSystem',
 })
 
